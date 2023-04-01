73bce7819afe80213558fdc7db0d54d555852361
raymond124 <19222432@life.hkbu.edu.hk>
2023-04-02 04:13:55 +0800

    Update the flashlight with a battery mechanism

--- Assets/FlashLightSystem.cs
+++ Assets/FlashLightSystem.cs
@@ -7,35 +7,54 @@ public class FlashLightSystem : MonoBehaviour
     [SerializeField] float lightDecay = .1f;
     [SerializeField] float angleDecay = 1f;
     [SerializeField] float minimumAngle = 40f;
-
-    Light myLight;
+    [SerializeField] private float batteryLife = 100f; // The battery life of the flashlight
+    [SerializeField] private float batteryDrain = 1f; // The amount of battery drained per second
+    
+    Light flashlight;
 
     private void Start()
     {
-        myLight = GetComponent<Light>();
+        flashlight = GetComponent<Light>();
     }
 
     private void Update()
     {
         DecreaseLightAngle();
         DecreaseLightIntensity();
+        batteryLoss();
     }
 
     private void DecreaseLightAngle()
     {
-        if (myLight.spotAngle <= minimumAngle)
+        if (flashlight.spotAngle <= minimumAngle)
         {
             return;
         }
         else
         {
-            myLight.spotAngle -= angleDecay * Time.deltaTime;
+            flashlight.spotAngle -= angleDecay * Time.deltaTime;
         }
     }
 
     private void DecreaseLightIntensity()
     {
-        myLight.intensity -= lightDecay * Time.deltaTime;
+        flashlight.intensity -= lightDecay * Time.deltaTime;
     }
 
+    private void batteryLoss(){
+        if (batteryLife > 0) // If the flashlight is on and the battery still has life left
+        {
+            flashlight.enabled = true; // Enable the light component
+            batteryLife -= batteryDrain * Time.deltaTime; // Drain the battery over time
+        }
+        else
+        {
+            flashlight.enabled = false; // Disable the light component
+        }
+    }
+
+    public void RechargeBattery(float rechargeAmount)
+    {
+        batteryLife += rechargeAmount;
+    }
 }
