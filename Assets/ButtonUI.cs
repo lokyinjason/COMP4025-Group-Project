d678888c5190e309fbbf600e3b2782437369aa8d
lokyinjason <lokyinjason921@gmail.com>
2023-04-18 09:59:57 +0800

    Start menu

---
+++ Assets/ButtonUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonUI : MonoBehaviour
+{
+    public void startGame() {
+        SceneManager.LoadScene(1);
+    }
+}
